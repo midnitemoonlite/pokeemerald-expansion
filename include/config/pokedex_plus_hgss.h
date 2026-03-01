afd852e8331ee7c9f4843fc2099d3fa5cc8d0922
midnitemoonlite <dylannordhagen@hotmail.com>
2026-03-01 18:43:14 -0500

    Add darkest Pokedex

--- include/config/pokedex_plus_hgss.h
+++ include/config/pokedex_plus_hgss.h
@@ -3,10 +3,10 @@
 
 #define POKEDEX_PLUS_HGSS                   TRUE // If TRUE, enables the custom HGSS style Pokedex.
 #define HGSS_DECAPPED                       FALSE // If TRUE, uses decapped gfx and strings.
-#define HGSS_DARK_MODE                      FALSE // If TRUE, enables dark mode.
 #define HGSS_HIDE_UNSEEN_EVOLUTION_NAMES    TRUE // If TRUE, hides evolution mon names.
 #define HGSS_HIDE_UNOWNED_EVOLUTION_METHODS FALSE // If TRUE, hides evolution methods.
 #define HGSS_SORT_TMS_BY_NUM                FALSE // If TRUE, sorts the TMS in HGSS Dex by TM number, rather than alphabetically.
 #define HGSS_SHOW_EGG_MOVES_FOR_EVOS        TRUE // If TRUE, shows Egg Moves for evolved Pokémon too.
+#define HGSS_COLOR_MODE                     HGSS_DARKEST // Color theme of HGSS dex
 
 #endif // GUARD_CONFIG_POKEDEX_PLUS_HGSS_H
